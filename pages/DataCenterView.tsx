bccce471e62fca1678f956f7823f40da27538154
Antonio Augusto da Silva <antonioaugustomg@hotmail.com>
2026-01-05 09:56:43 -0300

    feat: ADD USUARIOS

--- pages/DataCenterView.tsx
+++ pages/DataCenterView.tsx
@@ -14,6 +14,10 @@ const MOCK_USERS: User[] = [
     { id: 'u2', name: 'Cintia Ferreira', role: 'Engenheira Orçamentista', email: 'cintia.ferreira@grupodr.com.br' },
     { id: 'u3', name: 'Tatiana Guimarães', role: 'Engenheira Auxiliar', email: 'tatiana.guimaraes@grupodr.com.br' },
     { id: 'u4', name: 'Nilton Camilo', role: 'Gerente Comercial', email: 'nilton.camilo@grupodr.com.br' },
+    { id: 'u5', name: 'Maria Tereza', role: 'Engenheiro Trainee', email: 'maria.tereza@grupodr.com.br' },
+    { id: 'u6', name: 'Isabela Costa', role: 'Assistente Comercial', email: 'isabela.costa@grupodr.com.br' },
+    { id: 'u7', name: 'Fabiana Fernandes', role: 'Analista Comercial Jr', email: 'fabiana.fernandes@grupodr.com.br' },
+    { id: 'u8', name: 'Clara Santos', role: 'Jovem Aprendiz', email: 'clara.santos@grupodr.com.br' },
 ];
 
 export const DataCenterView: React.FC = () => {
