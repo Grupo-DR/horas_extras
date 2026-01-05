95e3fc87ab4d8d012630bbe3f417f605f57c272e
Antonio Augusto da Silva <antonioaugustomg@hotmail.com>
2026-01-05 04:58:44 -0300

    feat: Revisões Estéticas 3

--- layout/MainLayout.tsx
+++ layout/MainLayout.tsx
@@ -6,7 +6,7 @@ export const MainLayout: React.FC = () => {
     return (
         <div className="flex min-h-screen bg-[#F0F4F8] font-sans selection:bg-blue-100 selection:text-blue-700 overflow-x-hidden">
             <Sidebar />
-            <main className="flex-1 pl-64 min-h-screen w-full relative">
+            <main className="flex-1 pl-20 min-h-screen w-full relative">
                 <div className="absolute inset-0 bg-gradient-to-tr from-blue-50/50 via-white/20 to-indigo-50/50 pointer-events-none -z-10" />
                 <Outlet />
             </main>
